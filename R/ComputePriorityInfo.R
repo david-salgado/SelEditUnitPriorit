cc5c567df3ccffd1183858f9f9fba53bbffa1729
PC_Metodologia1 <david.salgado.fernandez@ine.es>
2018-04-13 14:42:17 +0200

    Change in CompuyePriorityInfo.

--- R/ComputePriorityInfo.R
+++ R/ComputePriorityInfo.R
@@ -101,15 +101,15 @@ setMethod(
             setnames(localOutput, 'UnitScoreQuantile', UnitToIDDDNames('CuantGlob', DD))
             #setnames(localOutput, paste0('PredError', VarName), paste0('Parametro_07._5.1.1.7._', VarName))
             # setnames(localOutput, paste0('ObsErrorSTD', VarName), paste0('Parametro_07._5.1.1.8._', VarName))
-            setnames(localOutput, paste0('ObsErrorSTD', VarName), paste(UnitToIDDDNames('STDErrorObs', DD), IDDDToUnitNames(VarName, DD), sep = '_'))
+            setnames(localOutput, paste0('ObsErrorSTD', VarName), paste(UnitToIDDDNames('STDErrorObs', DD), names(VarName), sep = '_'))
             #setnames(localOutput, paste0('PredErrorSTD', VarName), paste0('Parametro_07._5.1.1.8._', VarName))
             # setnames(localOutput, paste0('PredErrorSTD', VarName), paste0('Parametro_07._5.1.1.7._', VarName))
-            setnames(localOutput, paste0('PredErrorSTD', VarName), paste(UnitToIDDDNames('STDErrorPred', DD), IDDDToUnitNames(VarName, DD), sep = '_'))
+            setnames(localOutput, paste0('PredErrorSTD', VarName), paste(UnitToIDDDNames('STDErrorPred', DD), names(VarName), sep = '_'))
             # setnames(localOutput, paste0('MomentQuant', VarName), paste0('Parametro_07._5.1.1.9._', VarName))
-            setnames(localOutput, paste0('MomentQuant', VarName), paste(UnitToIDDDNames('CuantMom', DD), IDDDToUnitNames(VarName, DD), sep = '_'))
+            setnames(localOutput, paste0('MomentQuant', VarName), paste(UnitToIDDDNames('CuantMom', DD), names(VarName), sep = '_'))
             # setnames(localOutput, paste0('Pred', VarName), paste0('Parametro_07._5.1.1.10._', VarName))
-            setnames(localOutput, paste0('Pred', VarName), paste(UnitToIDDDNames('PredValue', DD), IDDDToUnitNames(VarName, DD), sep = '_'))
-            
+            setnames(localOutput, paste0('Pred', VarName), paste(UnitToIDDDNames('PredValue', DD), names(VarName), sep = '_'))
+
             output[[VarName]] <- localOutput
 
         }
