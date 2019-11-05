8c64eea5a8e4c79b057d6983be36991339c11300
david-salgado <david.salgado.fernandez@gmail.com>
2019-11-05 10:09:49 +0100

    Bug corrected in PrioritizeUnits

--- R/PrioritizeUnits.R
+++ R/PrioritizeUnits.R
@@ -32,22 +32,35 @@ setMethod(f = "PrioritizeUnits",
 
               VarNames <- slot(object, 'VarNames')
               nCol <- length(VarNames)
-              UnitScores <- lapply(seq(along = object@Moments), function(indexArray){
+              UnitScores <- lapply(seq(along = object@Units), function(cellIndex){
 
-                  nRow <- object@Moments[[indexArray]]$dim[1]
-                  Mat <- matrix(NA_real_,
-                                nrow = nRow,
-                                ncol = nCol,
-                                dimnames = list(NULL, VarNames))
-                  for (i in seq(along = VarNames)){
-
-                      indexSubset <- ((i-1) * nRow + 1):(i * nRow)
-                      Mat[, i] <- object@Moments[[indexArray]]$v[indexSubset]
-                  }
-                  output <- do.call(Param@UnitScFunction, c(list(x = Mat), Param@ScFunctionParam))
-                  return(output)
-
+                  array <- object@Moments[[cellIndex]]
+                  moments <- array$v
+                  cellDim <- array$dim[[1]]
+                  tempList <- split(moments, rep(1:nCol, cellDim))
+                  names(tempList) <- VarNames
+                  tempDT <- as.data.table(tempList)[
+                      , globalSc := do.call(Param@UnitScFunction, c(list(x = as.matrix(.SD)), Param@ScFunctionParam)), .SDcols = VarNames]
+                  gbScore <- tempDT[['globalSc']]
+                  return(gbScore)
               })
+
+              # UnitScores <- lapply(seq(along = object@Moments), function(indexArray){
+              #
+              #     nRow <- object@Moments[[indexArray]]$dim[1]
+              #     Mat <- matrix(NA_real_,
+              #                   nrow = nRow,
+              #                   ncol = nCol,
+              #                   dimnames = list(NULL, VarNames))
+              #     for (i in seq(along = VarNames)){
+              #
+              #         indexSubset <- ((i-1) * nRow + 1):(i * nRow)
+              #         Mat[, i] <- object@Moments[[indexArray]]$v[indexSubset]
+              #     }
+              #     output <- do.call(Param@UnitScFunction, c(list(x = Mat), Param@ScFunctionParam))
+              #     return(output)
+              #
+              # })
 
               UnitPriority <- lapply(UnitScores, function(score){
 
@@ -55,26 +68,26 @@ setMethod(f = "PrioritizeUnits",
 
                   if (length(Param@DesignW) == 0){
 
-                    setkeyv(dt.aux, 'scores')
-                    dt.aux[, priority := rev(seq(along = score))]
-                    setkey(dt.aux, 'init.order')
+                      setkeyv(dt.aux, 'scores')
+                      dt.aux[, priority := rev(seq(along = score))]
+                      setkey(dt.aux, 'init.order')
 
-                    out <- dt.aux[['priority']]
-                    return(out)
+                      out <- dt.aux[['priority']]
+                      return(out)
 
                   } else {
 
-                    for (indexVar in seq(along = object@Moments)){
+                      for (indexVar in seq(along = object@Moments)){
 
-                        dt.aux[, paste0('DesignW', VarNames[indexVar]) := -Param@DesignW[[ VarNames[indexVar] ]]]
+                          dt.aux[, paste0('DesignW', VarNames[indexVar]) := -Param@DesignW[[ VarNames[indexVar] ]]]
 
-                    }
-                    dt.aux[, scores := -scores]
-                    setkeyv(dt.aux, c('scores', paste0('DesignW', VarNames)))
-                    dt.aux[, priority := seq(along = score)]
-                    setkey(dt.aux, 'init.order')
-                    out <- dt.aux[['priority']]
-                    return(out)
+                      }
+                      dt.aux[, scores := -scores]
+                      setkeyv(dt.aux, c('scores', paste0('DesignW', VarNames)))
+                      dt.aux[, priority := seq(along = score)]
+                      setkey(dt.aux, 'init.order')
+                      out <- dt.aux[['priority']]
+                      return(out)
 
                   }
 
